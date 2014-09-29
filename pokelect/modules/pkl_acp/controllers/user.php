7554af14675abfb3e65e45f77e3e289c91e4d59a
Paul 'Ninj4' Chater <developer@paulchater.co.uk>
2014-09-29 19:31:59 +0100

    Updated lots of stuff.

---
+++ pokelect/modules/pkl_acp/controllers/user.php
@@ -0,0 +1,32 @@
+<?php
+
+class User extends MX_Controller {
+	
+		private function __construct()
+		{
+			parent::__construct();
+		}
+
+		public function home()
+		{
+			/*
+			 *
+			 * TODO: 
+			 *  
+			 *  SELECT * FROM users WHERE id=*
+			 *	Implement User Dashboard this will list all the users.
+			 *	With a quick and intuitive way to update users.
+			 *
+			 */
+		}
+
+		private function delete($id=false);
+		{
+
+		}
+
+		private function create();
+		{
+
+		}
+}
